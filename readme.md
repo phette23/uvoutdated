1635aec94eb7b0f17386c92b32d14adf778544d9
phette23 <phette23@gmail.com>
2025-02-05 16:47:02 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# `uvoutdated`
+
+Like `uv pip list --outdated` except only for direct dependencies listed in pyproject.toml.
+
+## License
+
+[CC0 Public Domain](https://creativecommons.org/publicdomain/zero/1.0/legalcode)
---
+++ uvoutdated.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from pathlib import Path
+import re
+from subprocess import check_output
+from typing import Any
+
+import click
+import toml
+
+
+@click.command(name="uvoutdated")
+@click.help_option("-h", "--help")
+@click.argument(
+    "file",
+    default=Path(".") / "pyproject.toml",
+    metavar="pyproject.toml",
+    type=click.Path(dir_okay=False, exists=True, readable=True),
+)
+def main(file: Path):
+    """Print outdated direct dependencies in pyproject.toml."""
+    project: dict[str, Any] = toml.load(file)["project"]
+    deps: list[str] = project["dependencies"]
+    click.echo(outdated(deps), nl=False)
+
+
+def outdated(deps: list[str]) -> str:
+    output: str = ""
+    # TODO cd to parent dir of path before running?
+    outdated: str = check_output(["uv", "pip", "list", "--outdated"]).decode("utf-8")
+    for index, line in enumerate(outdated.split("\n")):
+        if index < 2:
+            output += f"{line}\n"  # header rows
+        else:
+            pkg: str = line.split(" ")[0]
+            for dep in deps:
+                if re.match(r"^" + pkg, dep):
+                    output += f"{line}\n"
+                    break
+    return output
+
+
+if __name__ == "__main__":
+    main()
